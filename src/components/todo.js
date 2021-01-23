bd4f0866630c2038d053320627355a76ab7dbf9d
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-01-22 22:14:25 -0700

    updated sortbar styling

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,55 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 import useAjaxCalls from './hooks/ajax';
 import './todo.scss';
 // const URL = 'https://api-js401.herokuapp.com/api/v1/todo';
 
 function ToDo(props) {
-
   const [URL, list, setList, addItem] = useAjaxCalls();
   const [count, setCount] = useState();
 
   useEffect(() => {
-    setCount(list.filter(item => !item.complete).length)
+    setCount(list.filter(item => !item.complete).length);
   }, [list]);
 
-  const toggleComplete = async(id) => {
+  const toggleComplete = async id => {
     let item = list.filter(i => i._id === id)[0] || {};
-    if(item._id){
-      const res = await axios.put(`${URL}/${id}`, {"complete": !item.complete})
+    if (item._id) {
+      const res = await axios.put(`${URL}/${id}`, { complete: !item.complete });
       let removeId = item._id;
-      let newList = list.filter((item) => item._id !== removeId)
+      let newList = list.filter(item => item._id !== removeId);
       setList([...newList, res.data]);
     }
   };
-    const deleteItem = id => {
-      let item = list.filter(i => i._id === id)[0] || {};
-      if(item._id){
-        axios.delete(`${URL}/${item._id}`, )
-        let removeId = item._id;
-        let newList = list.filter((item) => item._id !== removeId)
-        setList([...newList]);
-      }
-      
+  const deleteItem = id => {
+    let item = list.filter(i => i._id === id)[0] || {};
+    if (item._id) {
+      axios.delete(`${URL}/${item._id}`);
+      let removeId = item._id;
+      let newList = list.filter(item => item._id !== removeId);
+      setList([...newList]);
     }
-    
+  };
+
   return (
-    
     <div>
-
-      <Container fluid id="container">
-        <Row>
-          <Col>There are {count} Items To Complete</Col>
-        </Row>
-      </Container>
-
+      <div className="statusbar">There are {count} Items To Complete</div>
       <section className="todo">
-
         <div>
           <TodoForm handleSubmit={addItem} />
         </div>
@@ -65,6 +52,5 @@ function ToDo(props) {
     </div>
   );
 }
-
 
 export default ToDo;
